aa0f5416d495f0cc7d07345fd4d4456482dc9ae5
Evgeniya <evavong363@gmail.com>
2022-11-19 16:25:15 +0300

    Added Two Frends and Dog

---
+++ Example008-TwoFriends&Dog/Program.cs
@@ -0,0 +1,24 @@
+﻿// Two friends and the dog
+int speedFirstFriend=1000,
+    speedSecondFriend=2000,
+    speedDog=5000, Friend=2,
+    count=0;
+double distance=10000, time = 0;
+  
+while(distance>1)
+{
+  if(Friend==1)
+  {
+    time = distance / (speedSecondFriend + speedDog);
+    Friend=2;
+  }
+  else
+  {
+    time = distance / (speedFirstFriend + speedDog);
+    Friend=1;
+  }
+  distance = distance-time*(speedFirstFriend+speedSecondFriend);
+  count++;
+}
+
+Console.WriteLine("Собака пробежала (раз): "+count);
