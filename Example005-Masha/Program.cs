d7bb4b3f038eeda3127b3ac4ce18c6f8bec0cdd2
Evgeniya <evavong363@gmail.com>
2022-11-21 11:35:01 +0300

    --amend

--- Example005-Masha/Program.cs
+++ Example005-Masha/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Маша? 
 Console.Write("Представьтесь: ");
-string username = Console.ReadLine();
+string username = Console.ReadLine() ?? string.Empty;
 if (username.ToLower()=="маша")
 {
   Console.Write("Ура! ");
