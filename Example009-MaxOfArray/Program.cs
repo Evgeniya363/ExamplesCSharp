5bd323f8ef33673bde49f2f3dc3dc9ac4efb9b48
Evgeniya <evavong363@gmail.com>
2022-11-22 21:31:07 +0300

    Added Find MAX in the array

---
+++ Example009-MaxOfArray/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти максимальный элемент массива
+
+int[] array = { 12, 42, 75, 92, 35, 76, 98, 34, 56, 34 };
+int count = array.Length;
+int max = array[0];
+int i = 1;
+
+Console.WriteLine("Исходный массив: ");
+while (i < count)
+{
+  Console.Write($"{array[i]}  ");
+  if (array[i] > max) max = array[i];
+  i++;
+}
+Console.WriteLine($"\nМаксимальный элемент: {max}");
