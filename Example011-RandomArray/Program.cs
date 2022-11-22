899f57852c3ee6dbca6749696cf8adb471dc9e86
Evgeniya <evavong363@gmail.com>
2022-11-22 22:18:51 +0300

    Added Random array

---
+++ Example011-RandomArray/Program.cs
@@ -0,0 +1,28 @@
+﻿// Создание и вывод массива случайныз чисел
+void FillArray (int[] collection)
+{
+  int length = collection.Length;
+  int index = 0;
+  while( index < length)
+  {
+    collection[index] = new Random().Next(1,100);
+    index++;
+  }
+}
+
+void PrintArray(int[] collection)
+{
+  int length = collection.Length;
+  int index = 0;
+  Console.WriteLine("Массив случайных чисел: ");
+  while (index < length)
+  {
+    Console.Write($"{collection[index]} ");
+    index++;
+  }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
