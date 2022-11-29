db99d3fac513c8668a117509b39aad36bc3af734
Evgeniya <evavong363@gmail.com>
2022-11-29 21:01:42 +0300

    Added Collection Sort by Max

---
+++ Example014-SortByMax/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] arr = { 1, 3, 8, 6, 2, 5, 6, 2, 9, 6, 4 };
+
+void PrintArray(int[] array)
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    Console.Write($"{array[i]}  ");
+  }
+}
+
+void CollectionSort(int[] array)
+{
+  for (int i = array.Length-1; i>1; i--)
+  {
+    int maxPosition = i;
+    for (int j = i - 1; j >0; j--)
+    {
+      if (array[j]>array[maxPosition]) maxPosition = j;
+    }
+    if (i != maxPosition)
+    {
+      int tmp = array[i];
+      array[i] = array[maxPosition];
+      array[maxPosition] = tmp;
+    }
+  }
+}
+
+CollectionSort(arr);
+PrintArray(arr);
