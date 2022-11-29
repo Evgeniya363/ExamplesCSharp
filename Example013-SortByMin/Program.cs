88a704edd04de71c9bea9564544af4095d8059ce
Evgeniya <evavong363@gmail.com>
2022-11-29 20:38:33 +0300

    Added Collection Sort by Min

---
+++ Example013-SortByMin/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] arr = {1,3,8,6,2,5,6,2,9,6,4};
+
+void PrintArray(int[] array)
+{
+  for (int i=0; i<arr.Length; i++)
+  {
+    Console.Write($"{array[i]}  ");
+  }
+}
+
+void CollectionSort(int[] array)
+{
+  for (int i=0; i<array.Length-1; i++)
+  {
+    int minPosition=i;
+    for (int j=i+1; j<array.Length; j++)
+    {
+      if (array[j]< array[minPosition]) minPosition=j;
+    }
+    if (i!=minPosition)
+    {
+      int tmp=array[i];
+      array[i]=array[minPosition];
+      array[minPosition]=tmp;
+    }
+  }
+}
+
+CollectionSort(arr);
+PrintArray(arr);
