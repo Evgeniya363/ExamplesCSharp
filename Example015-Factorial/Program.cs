b67a95e0f52ca47e3dbf18e47134a3622beb61cf
Evgeniya <evavong363@gmail.com>
2022-12-07 15:21:42 +0300

    Added Examples015-017

---
+++ Example015-Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿// int factorial(int N)
+// {
+//   if (N == 1) return 1;
+//   else return N * factorial(N - 1);
+// }
+
+// Console.Write("Введите n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"{n}! = {factorial(n)}");
+
+
+double factorial(double N)
+{
+    if (N==1) return 1;
+    else return N * factorial(N-1);
+}
+
+for (double i=1; i<=40; i++)
+{
+    Console.WriteLine($"{i}! = {factorial(i)}");
+}
+
---
+++ Example016-Fibonacci/Program.cs
@@ -0,0 +1,15 @@
+﻿int fibonacci(int N)
+{
+  if (N == 1 || N ==  2) return 1; 
+  else return fibonacci(N - 1) + fibonacci(N - 2);
+}
+
+//Console.Write("Введите n: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine($"f({n}) = {fibonacci(n)}");
+
+for (int n = 1; n <= 40; n++)
+{
+  Console.WriteLine($"f({n}) = {fibonacci(n)}");
+}
